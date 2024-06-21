b7ba6470f68a520cd36bae9b9b3a20ebf265c1cf
Jalen-Moore <jrmoore@my.loyno.edu>
2024-06-21 16:35:12 -0500

    Updated CSS for Flashcard

--- frontend/gemini-dynamo/src/Flashcard.jsx
+++ frontend/gemini-dynamo/src/Flashcard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Flashcard({term, definition, onDiscard}) {
     return (
-        <div className='flashcard' style={{border: 'silver'}}>
+        <div className='flashcard'>
             <h3>{term}</h3>
             <p>{definition}</p>
             <button onClick={onDiscard} style={{marginTop: '10px'}}>
